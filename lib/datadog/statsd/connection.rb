76d0bac2010b831c621c4b2296e9b4c6303f3fc1
nakedsushi <louise@art19.com>
2020-05-12 15:57:45 -0700

    PLAT-425 #time 5m fix missing begin

--- lib/datadog/statsd/connection.rb
+++ lib/datadog/statsd/connection.rb
@@ -25,36 +25,38 @@ module Datadog
 
       def write(payload)
         synchronize do
-          logger.debug { "Statsd: #{payload}" } if logger
+          begin
+            logger.debug { "Statsd: #{payload}" } if logger
 
-          flush_telemetry = telemetry.flush?
+            flush_telemetry = telemetry.flush?
 
-          payload += telemetry.flush if flush_telemetry
+            payload += telemetry.flush if flush_telemetry
 
-          send_message(payload)
+            send_message(payload)
 
-          telemetry.reset if flush_telemetry
+            telemetry.reset if flush_telemetry
 
-          telemetry.sent(packets: 1, bytes: payload.length)
-        rescue StandardError => boom
-          # Try once to reconnect if the socket has been closed
-          retries ||= 1
-          if retries <= 1 &&
-            (boom.is_a?(Errno::ENOTCONN) or
-             boom.is_a?(Errno::ECONNREFUSED) or
-             boom.is_a?(IOError) && boom.message =~ /closed stream/i)
-            retries += 1
-            begin
-              close
-              retry
-            rescue StandardError => e
-              boom = e
+            telemetry.sent(packets: 1, bytes: payload.length)
+          rescue StandardError => boom
+            # Try once to reconnect if the socket has been closed
+            retries ||= 1
+            if retries <= 1 &&
+              (boom.is_a?(Errno::ENOTCONN) or
+                boom.is_a?(Errno::ECONNREFUSED) or
+                boom.is_a?(IOError) && boom.message =~ /closed stream/i)
+              retries += 1
+              begin
+                close
+                retry
+              rescue StandardError => e
+                boom = e
+              end
             end
-          end
 
-          telemetry.dropped(packets: 1, bytes: payload.length)
-          logger.error { "Statsd: #{boom.class} #{boom}" } if logger
-          nil
+            telemetry.dropped(packets: 1, bytes: payload.length)
+            logger.error { "Statsd: #{boom.class} #{boom}" } if logger
+            nil
+          end
         end
       end
 
